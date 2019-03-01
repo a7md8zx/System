1e9b5711f2de7cafc5892014d4f30ff4f6d04e0a
a7md8zx <42849941+a7md8zx@users.noreply.github.com>
2019-03-01 17:23:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.login("NTQ4ODgxMTc5NDg3NTAyMzQ2.D1Md_A.z6ascOZznE_dzGTknxUmoCKN_Ek");
+
+ 
+const prefix = '+' // ادممن بريفكس
+ 
+ 
+const developers = ["370957127893647360"] // ايدي صاحب البوت
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+     
+  if (message.content.startsWith(prefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+     if (message.content === (prefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(prefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(prefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(prefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+ 
