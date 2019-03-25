d2a516380d11058b8e9c7ee77629e965b03a08c1
a7md8zx <42849941+a7md8zx@users.noreply.github.com>
2019-03-25 23:59:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,41 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.login("NTQ4ODgxMTc5NDg3NTAyMzQ2.D1Md_A.z6ascOZznE_dzGTknxUmoCKN_Ek");
-
+const prefix = "="
  
-const prefix = '+' // ادممن بريفكس
+client.on('ready', () => {
+console.log(`سجل الدخول ${client.user.tag}!`);
+console.log('')
+console.log('')
+console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+console.log(`[Start] ${new Date()}`);
+console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+console.log('')
+console.log('╔[════════════════════════════════════]╗');
+console.log(`تسجيل الدخول ل * [ " ${client.user.username} " ]`);
+console.log('')
+console.log('معلومات : ')
+console.log('')
+console.log(`عدد السيرفرات♤ [ " ${client.guilds.size} " ]`);
+console.log(`المستخدمين♡ [ " ${client.users.size} " ]`);
+console.log(`الرومات♧ [ " ${client.channels.size} " ]`);
+console.log('╚[════════════════════════════════════]╝')
+console.log('By Jackeo')
+console.log('╔[════════════]╗')
+console.log(' البوت اون لاين')
+console.log('╚[════════════]╝')
+console.log('V2 العربية arap ')
+console.log('Dev Narox by Jackeo')
  
  
-const developers = ["370957127893647360"] // ايدي صاحب البوت
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-     
-  if (message.content.startsWith(prefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-     if (message.content === (prefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(prefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-  if (message.content.startsWith(prefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-  if (message.content.startsWith(prefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith(prefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(prefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
+//النسخة المعربة من جاكيو
+ 
+ 
 });
  
+//حط الاكواد تحت ↓↓↓
+ 
+ 
+ 
+//هنا التوكن اذا ما تبغا لهيروكو شيلprocess.env.BOT_TOKEN وحط توكنك بدالها
+ 
+client.login(NTU4NzY2MzA0MTk1MTE3MDc2.D3rSgA.oLjraWfr0aTAYm6rFcUL3uouIQ0);
